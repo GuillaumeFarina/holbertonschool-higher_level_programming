ed0025cf6f0f83d364888bf800431d35414c6c13
Wqkz <guillaume.farina@hotmail.com>
2024-09-03 21:54:45 +0200

    Finally fix code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-for index in range(0, 10):
-    for index_2 in range(1, 10):
-        if index >= index_2:
-            continue
-        elif index == 8 and index_2 == 9:
-            print("{}{}".format(index, index_2))
-        else:
-            print("{}{}, ".format(index, index_2), end="")
+for index in range(0, 100):
+    if index < 99:
+        print("{:02}".format(index), end=", ")
+    else:
+        print("{:02}".format(index))
