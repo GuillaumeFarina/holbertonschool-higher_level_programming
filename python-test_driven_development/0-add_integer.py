36f45cd8badd7e03400ecaa848810172371e0b4b
Wqkz <guillaume.farina@hotmail.com>
2024-09-11 15:47:45 +0200

    fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-    function called add_integer that takes two arguments, a and b. The argument b has a default value of 98.
+    function called add_integer that takes two arguments, a and b.
 
-    checks if the type of a is not int or float. If it’s not, it raises a TypeError with the message “a must be an integer” or “b must be an integer”.
+    checks if the type of a is not int or float. If it’s not.
 """
+
+
 def add_integer(a, b=98):
     """
     converts a and b to integers (int) and returns their sum.
     """
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
