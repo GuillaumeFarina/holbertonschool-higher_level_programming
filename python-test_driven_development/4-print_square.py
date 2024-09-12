85aff293a967608df3192ae94b0da7b14918e5aa
Wqkz <guillaume.farina@hotmail.com>
2024-09-12 16:25:23 +0200

    add comments and fix pycodestyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+function that prints a square with the character #.
+"""
+
 
 def print_square(size):
+    """
+    Check if the variable 'size' is neither an integer nor a float
+    and if it is less than 0
+
+    Check if 'size' is less than 0
+
+    Loop to print a line of '#' of length 'size', 'size' times
+    """
+
     if type(size) is not [int] is float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
