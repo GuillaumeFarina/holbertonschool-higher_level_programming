da911b05a59ac60219ace32a8590f70650c018fe
Wqkz <guillaume.farina@hotmail.com>
2024-09-16 16:22:17 +0200

    Add a Size value and Write a class Square with 2 differents Errors

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a module for Square class
+"""
+
+
+class Square:
+    """
+    This is a class named Square
+    """
+    def __init__(self, size=0):
+        """
+        Private instance attribute of parameter value"size=0"
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
