c07c9959106570e8af8714ee50a0d9699eeb49a1
Wqkz <guillaume.farina@hotmail.com>
2024-09-16 15:58:52 +0200

    Add Private instance attribute of "size"

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a module for Square class
+"""
+
+
+class Square:
+    """
+    This is a class named Square
+    """
+    def __init__(self, size):
+        """
+        Private instance attribute of parameter "size"
+        """
+        self.__size = size
