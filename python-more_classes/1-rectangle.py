c2cc710cfb32696c92d45200f6be266c8650a24a
Wqkz <guillaume.farina@hotmail.com>
2024-09-17 10:59:17 +0200

    Write Class Rectangle and add 2 Private instance attribute

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This is a module for Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    This is a class named Rectangle
+    """
+    @property
+    def width(self):
+        self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+
+        self.height = height
+        self.width = width
