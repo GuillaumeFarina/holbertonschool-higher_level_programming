9069904c8aa8af2179e1a1e574a0dd810fcd62fb
Wqkz <guillaume.farina@hotmail.com>
2024-09-23 08:56:28 +0200

    add comment on my code

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This is a docstring for the module, which is currently empty
+"""
+
 
 def lookup(obj):
+    """
+    This function takes an object as an argument and
+    returns a list of its attributes and methods
+    """
     return dir(obj)
