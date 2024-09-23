c782371a21552c4066dc9f9ecbd23a1fe584c1af
Wqkz <guillaume.farina@hotmail.com>
2024-09-23 11:00:27 +0200

    Add description of my Class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@ This is a docstring for the module, which is currently empty
 
 
 class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+
     def print_sorted(self):
         """
         Prints the list in ascending sorted order.
