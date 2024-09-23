c259e4f76f39e6aad4815a338f422d0e07531ca5
Wqkz <guillaume.farina@hotmail.com>
2024-09-23 16:40:02 +0200

    try to fix code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -33,5 +33,5 @@ class Rectangle(BaseGeometry):
         """initialize width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        __width = width
+        __height = height
