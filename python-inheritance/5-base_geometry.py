4c601b6ffba6b36d066e4cfcb8f0ca1924cb41ae
Wqkz <guillaume.farina@hotmail.com>
2024-09-23 14:03:22 +0200

    Write an empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a docstring for the module, which is currently empty
+"""
+
+
+class BaseGeometry():
+    """
+    class List that inherits from list
+    """
+    pass
