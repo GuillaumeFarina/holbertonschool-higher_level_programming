42c9ab2cb27c7b57c738edc509b708fa9c8868a1
Wqkz <guillaume.farina@hotmail.com>
2024-09-23 16:42:40 +0200

    import my last file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
-"""
-This is a docstring for the module.
-"""
-
-
-class BaseGeometry():
-    """
-    class List that inherits from list
-    """
-    def area(self):
-        """
-        Raise Exception
-        """
-        if not isinstance(self, float):
-            raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Check if the value is an instance of int
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""This is a docstring for the module."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class used to represent a Rectangle, inheriting from BaseGeometry.
-    """
+    """A subclass inherits from BaseGeometry"""
+
     def __init__(self, width, height):
         """initialize width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        __width = width
-        __height = height
+        self.__width = width
+        self.__height = height
