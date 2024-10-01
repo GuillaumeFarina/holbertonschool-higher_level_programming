21e070ea6e37450495cf0efa89d82bd9020ab687
Wqkz <guillaume.farina@hotmail.com>
2024-10-01 12:02:14 +0200

    reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+# read (r)
+
+
+def read_file(filename=""):
+    # function who read the file
+    with open(filename, encoding="utf-8") as my_file_0:
+         print(my_file_0.read(), end="")
