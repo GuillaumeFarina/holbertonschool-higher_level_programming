61475ac446cb7a682697d635db8ccda2d32062ad
Wqkz <guillaume.farina@hotmail.com>
2024-10-01 12:05:35 +0200

    Change # into """ for comment

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# read (r)
+""" read (r) """
 
 
 def read_file(filename=""):
-    # function who read the file
+    """ function who read the file """
     with open(filename, encoding="utf-8") as my_file_0:
          print(my_file_0.read(), end="")
