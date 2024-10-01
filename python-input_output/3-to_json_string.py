6da1c1684bf5c232571c4578441655a938ad8ee8
Wqkz <guillaume.farina@hotmail.com>
2024-10-01 16:48:25 +0200

    returns an object (Python data structure) represented by a JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 3. JSON """
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object(str).
+    """
+    return json.dumps(my_obj)
