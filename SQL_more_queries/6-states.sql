026505ff4bc07c031bae16ea5ad5ee9bfc429c28
Wqkz <guillaume.farina@hotmail.com>
2024-10-29 15:46:12 +0100

    Creates the database hbtn_0d_usa and the table states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE states (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256));
