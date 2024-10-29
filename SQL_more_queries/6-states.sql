a97ed9acf0bc911f083fa2998b2b1de32338e15a
Wqkz <guillaume.farina@hotmail.com>
2024-10-29 15:49:21 +0100

    add "USE" for my new databases

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- Creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE states (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256));
