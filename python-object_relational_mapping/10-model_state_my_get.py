83ae3deb23cddd5d0e53b9c3b4a09f3ddd2c81e0
Wqkz <guillaume.farina@hotmail.com>
2024-11-01 23:14:48 +0100

    Prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Prints the State object with
+the name passed as argument from the database hbtn_0e_6_usa"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+
+    """Connect to the database"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """Add argv for search with name the state"""
+    instance = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if instance is None:
+        print("Not found")
+    else:
+        print("{}".format(instance.id))
+
+    session.close()
