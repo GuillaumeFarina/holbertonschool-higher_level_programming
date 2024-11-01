c595135f18313a30ef86f0a32cddf481f7850956
Wqkz <guillaume.farina@hotmail.com>
2024-11-01 23:01:41 +0100

    Fix pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 
-"""Lists all states from the database hbtn_0e_0_usa"""
-
+"""
+Script that connects to a MySQL database and retrieves all entries
+from the 'states' table.
+It uses command-line arguments for authentication,
+including the username, password, and database name.
+"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(
-        host='localhost',
+
+    db = MySQLdb.connect(
+        host="localhost",
         port=3306,
         user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        password=sys.argv[2],
+        database=sys.argv[3],
     )
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+    cursor = db.cursor()
 
-    for row in rows:
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
 
     cursor.close()
-    connection.close
+    db.close()
